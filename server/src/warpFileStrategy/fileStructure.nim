ef9832f6e5479e2251ef383d44e2dfc278eb2dbc
DESKTOP-9PTFMOR\ybaba <jmachang368@gmail.com>
2023-02-21 23:09:12 +0900

    保存しとく

--- server/src/warpFileStrategy/fileStructure.nim
+++ server/src/warpFileStrategy/fileStructure.nim
@@ -2,34 +2,35 @@ from uuids import nil
 
 type
   FileStructure* = ref object
-    fileHeader: FileHeader
+    fileHeader*: FileHeader
     recordSection: RecordSection
 
   FileHeader* = ref object
-    version:  byte
-    recordCount: int32
-    dataSectionSize: uint64
+    version*:  byte
+    recordCount*: int32
+    dataSectionSize*: uint64
 
   RecordSection* = ref object
     records: seq[Record]
     offset: uint16
     
   Record* = ref object
-    recordHeader: RecordHeader
-    record: seq[int32]
+    recordHeader*: RecordHeader
+    record*: string
   
   RecordHeader* = ref object
-    recordKey: uuids.UUID
-    recordSize: int32
+    recordKey*: uuids.UUID
+    recordSize*: uint64
+    userKey*: string
 
-proc newWarpFileStructure*(): FileStructure =
+proc newWarpFileInstance*(): FileStructure =
   var fileHeader = FileHeader(
     version: 1,
     recordCount: 0,
     dataSectionSize: 0
   )
 
-  var recordSection = RecordSection()
+  var recordSection = RecordSection(offset: 13)
 
   result = FileStructure(
     fileHeader: fileHeader,
